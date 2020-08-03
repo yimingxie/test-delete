a511a02a80ce8c41179b6f482f95f6b6296e7e9a
xieyiming <329715712@qq.com>
2020-08-03 10:12:30 +0800

    first

---
+++ src/views/TestDelete.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="test">
+    <div>测试删除</div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/views/test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="test">
+    <div>test.vue</div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
